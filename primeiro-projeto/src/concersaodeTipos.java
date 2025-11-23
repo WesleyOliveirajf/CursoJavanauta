8e2d7bfb9baddb968b252877171203d2f5de82fe
Wesley Oliveira <lojastioinfo@gmail.com>
2025-11-23 15:41:35 -0300

    feat: Adiciona exercícios iniciais de Java

---
+++ primeiro-projeto/src/concersaodeTipos.java
@@ -0,0 +1,16 @@
+public class concersaodeTipos {
+    public static void main(String[] args) {
+        int numero = 10;
+        double d = numero;
+
+        double d2 = 10.5;
+        int int2 = (int) d2;
+
+        System.out.println(int2);
+
+        String numeroStringer = "123";
+        Integer numeroConvertido = Integer.parseInt(numeroStringer);
+
+
+    }
+}
