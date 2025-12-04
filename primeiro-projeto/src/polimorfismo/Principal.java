7d309a11beb3e29923fbefc993c307c4fc4c15d6
Wesley Oliveira <139356104+WesleyOliveirajf@users.noreply.github.com>
2025-12-04 15:55:25 -0300

    Atualização do projeto - implementação de polimorfismo com classe Calculadora

---
+++ primeiro-projeto/src/polimorfismo/Principal.java
@@ -0,0 +1,23 @@
+package polimorfismo;
+
+public class Principal {
+    public static void main(String[] args) {
+        int sabaoPo = 1;
+        int mamao = 2;
+        double sabonete = 2.50;
+        double arroz = 5.90;
+        Integer batata = 6;
+        Integer cenoura = 7;
+        Integer melancia = 5;
+
+        // click do mouse esquerdo + ctrl
+        int total = Calculadora.somar(sabaoPo, mamao);
+        double totalDouble = Calculadora.somar(sabonete, arroz);
+        Integer totalInteger = Calculadora.somar(batata, cenoura, melancia);
+
+
+
+
+
+    }
+}
